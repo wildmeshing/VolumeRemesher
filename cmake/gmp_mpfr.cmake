f83223c0bfac902979a61d7121169934df3b77f3
Daniel Zint <daniel.zint@mail.de>
2026-07-14 12:29:17 +0200

    Update GMP.

--- cmake/gmp_mpfr.cmake
+++ cmake/gmp_mpfr.cmake
@@ -1,17 +1,57 @@
 if(WIN32)
-    message(STATUS "Third-party: downloading gmp + mpfr")
-
-    include(FetchContent)
-    FetchContent_Declare(
-        gmp_mpfr
-        URL https://github.com/CGAL/cgal/releases/download/v5.2.1/CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip
-        URL_MD5 247f4dca741c6b9a9be76286414070fa
+    message(STATUS "Trying to find gmp+mpfr system-wide... (Windows only)")
+    
+    find_path(GMP_TRY_FIND_INCLUDES
+    NAMES
+        gmp.h
+    PATHS
+        ${INCLUDE_INSTALL_DIR}
+    PATH_SUFFIXES
+        include
     )
-    FetchContent_MakeAvailable(gmp_mpfr)
 
-    # For CGAL and Cork
-    set(ENV{GMP_DIR} "${gmp_mpfr_SOURCE_DIR}/gmp")
-    set(ENV{MPFR_DIR} "${gmp_mpfr_SOURCE_DIR}/gmp")
+    find_library(GMP_TRY_FIND_LIBRARIES
+    NAMES
+        gmp
+        libgmp-10
+    PATHS
+        ${LIB_INSTALL_DIR}
+    PATH_SUFFIXES
+        lib
+    )
+
+    find_file(GMP_TRY_FIND_RUNTIME_LIB
+        NAMES
+            gmp.dll
+            gmp-10.dll
+            libgmp-10.dll
+        PATHS
+            ENV GMP_DIR
+            ${LIB_INSTALL_DIR}
+        PATH_SUFFIXES
+            bin
+            lib
+    )
+
+    if(GMP_TRY_FIND_INCLUDES AND GMP_TRY_FIND_LIBRARIES AND GMP_TRY_FIND_RUNTIME_LIB)
+        message(STATUS "System GMP found: \n  ${GMP_TRY_FIND_INCLUDES}\n  ${GMP_TRY_FIND_LIBRARIES}\n  ${GMP_TRY_FIND_RUNTIME_LIB}")
+    else()
+        message(WARNING "Third-party: downloading gmp + mpfr.")
+
+        include(CPM)
+        CPMAddPackage(
+            NAME gmp_mpfr
+            URL "https://github.com/CGAL/cgal/releases/download/v6.1.1/CGAL-6.1.1-win64-auxiliary-libraries-gmp-mpfr.zip"
+            DOWNLOAD_ONLY YES
+        )
+
+        # For CGAL and Cork
+        set(ENV{GMP_DIR} "${gmp_mpfr_SOURCE_DIR}/gmp")
+        set(ENV{MPFR_DIR} "${gmp_mpfr_SOURCE_DIR}/gmp")
+    endif()
+
+
+    
 else()
     # On Linux/macOS, gmp+mpfr should be installed system-wide
 endif()
