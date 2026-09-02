4ee879155f08659e7dc42b13ad00cb3f838e8da0
MarcoAttene <marco.attene@gmail.com>
2026-09-02 12:16:32 +0200

    Reverted dep to MarcoAttene/Delaunay3D and adjusted default file load in MSVC/DEBUG

--- src/VolumeRemesher/main.cpp
+++ src/VolumeRemesher/main.cpp
@@ -279,6 +279,12 @@ int main(int argc, char** argv)
     char bool_opcode = '0';
 
     if (argc < 2) {
+#if defined _MSC_VER && !defined NDEBUG
+        fileA_name = (char*)"..\\models\\Octocat.off";
+        fileB_name = (char*)"..\\models\\Octocat.bg.tet";
+        bool_opcode = 'U';
+        verbose = true;
+#else
         printf("\nUsage: mesh_generator [-v | -l | -s | -b | -t] [inputfile_A.off] [bool_opcode "
                "inputfile_B.off] [-e edges.off] [-p points.off]\n\n"
                "Defines the volume enclosed by the input OFF file(s) and saves a volume mesh to "
@@ -308,6 +314,7 @@ int main(int argc, char** argv)
                "Example:\n"
                "mesh_generator ant.off U pig.off\n");
         return 0;
+#endif
     }
 
     for (int i = 1; i < argc; i++) {
